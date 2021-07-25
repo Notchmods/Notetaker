2bbb457f5296f7f48d6016ac0466c3a1569f8469
Notchmods <Nomics3000@gmail.com>
2021-07-25 20:35:14 +1000

    I've created a note taking software

---
+++ Notecreator/Note creator/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Note_creator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = textBox2.Text;
+            var Files = File.Create("C://Users/User/Downloads/"+path);
+            Files.Close();
+            File.WriteAllText("C://Users/User/Downloads/"+path, textBox1.Text);
+            MessageBox.Show(path + " is saved in the Downloads file");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            var OpenFile = new System.IO.StreamReader(openFileDialog1.FileName);
+            textBox1.Text = OpenFile.ReadToEnd();
+        }
+    }
+}
